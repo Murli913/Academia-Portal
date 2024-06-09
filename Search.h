027ac036f22f9dd8fbeffa9783c1c2bd3be8ba54
Murli913 <murlitalreja913@gmail.com>
2024-06-09 18:53:00 +0530

    impelemnt the searchactivecourse function

--- Search.h
+++ Search.h
@@ -26,41 +26,29 @@ int searchStudent(const char* inputRollno){
 }
 
 
-// int searchFaculty(const char* inputUID){
-//     const char* fileName="Faculty.txt";
-//     int fd=open(fileName,O_RDONLY|O_CREAT,0666);
-//     if(fd==-1){
-//         printf("Unable to access Faculty database\n");
-//         return -1;
-//     }
-//     struct Faculty chkFaculty;
-//     while(read(fd,&chkFaculty,sizeof(chkFaculty))>0){
-//         if(strcmp(chkFaculty.facultyUID,inputUID)==0){
-//             //duplicate exists
-//             return 0;
-//         }
-//     }
-//     //no duplicate exists
-//     return 1;
-// }
-// int searchActiveCourse(const char* inputCourseCode){
-//     const char* fileName="Courses.txt";
-//     int fd=open(fileName,O_RDONLY|O_CREAT,0666);
-//     if(fd==-1){
-//         printf("Unable to access Course database\n");
-//         return -1;
-//     }
-//     struct Course chkCourse;
-//     while(read(fd,&chkCourse,sizeof(chkCourse))>0){
-//         if(strcmp(chkCourse.course_code,inputCourseCode)==0 &&
-//             chkCourse.status==1){
-//             //duplicate exists
-//             return 0;
-//         }
-//     }
-//     //no duplicate exists
-//     return 1;
-// }
+
+
+
+int searchActiveCourse(const char* inputCourseCode){
+    const char* fileName="Courses.txt";
+    int fd=open(fileName,O_RDONLY|O_CREAT,0666);
+    if(fd==-1){
+        printf("Unable to access Course database\n");
+        return -1;
+    }
+    struct Course chkCourse;
+    while(read(fd,&chkCourse,sizeof(chkCourse))>0){
+        if(strcmp(chkCourse.course_code,inputCourseCode)==0 &&
+            chkCourse.status==1){
+            //duplicate exists
+            return 0;
+        }
+    }
+    //no duplicate exists
+    return 1;
+}
+
+
 // int getStudentCount(const char* inputCourseCode){
 //     const char* fileName="Courses.txt";
 //     int fd=open(fileName,O_RDONLY|O_CREAT,0666);
